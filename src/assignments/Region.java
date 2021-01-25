59ceba0a5f9bf6b57f07e8eaf2df9aae7a913332
Simon de Boer <simonwdeboer@gmail.com>
2021-01-25 10:38:18 +0100

    updated Region.java according to new file from Canvas

--- src/assignments/Region.java
+++ src/assignments/Region.java
@@ -1,84 +1,76 @@
 package assignments;
 
 import java.util.LinkedList;
-import java.util.Random;
-
 import umontreal.ssj.randvar.ExponentialGen;
 import umontreal.ssj.rng.RandomStream;
 import umontreal.ssj.simevents.Event;
 
 /**
- *
- * @author mctenthij
+ * @author A van Oostveen
+ * Edited by J Berkhout
  */
 public class Region {
-    LinkedList<Accident> queue;     //Queue of the server
-    LinkedList<Ambulance> idleAmbulances;  // Available ambulance
-    double [] baseLocation;
-    ArrivalProcess arrivalProcess;
-    RandomStream locationStream;
-    int regionID;
+
+    // region variables
+	LinkedList<Accident> queue; // for the unaddressed accidents
+	LinkedList<Ambulance> idleAmbulances;
+	double[] baseLocation; // of the particular region
+	ArrivalProcess arrivalProcess;
+	int regionID;
+	Region[] regions;
+	int numRegions;
+
+	RandomStream locationStream; // random number generator for locations
     
-    public Region(double baseXCoordinate, double baseYCoordinate, RandomStream rng, double arrivalRate, RandomStream location, int rid) {
-        queue = new LinkedList<>();
-        idleAmbulances = new LinkedList<>();
-        baseLocation = new double[2];
-        baseLocation[0] = baseXCoordinate;
-        baseLocation[1] = baseYCoordinate;
-        arrivalProcess = new ArrivalProcess(rng,arrivalRate);
-        locationStream = location;
-        regionID = rid;
-    }
+	public Region(int id, double[] baseLocation, RandomStream arrivalRandomStream, double arrivalRate, RandomStream locationRandomStream, Region[] regionArray, int numRegions) {
+
+		// set region variables
+		queue = new LinkedList<>();
+		idleAmbulances = new LinkedList<>();
+		this.baseLocation = baseLocation;
+		regionID = id;
+		this.regions = regionArray;
+		this.numRegions = numRegions;
+
+		// set random streams
+		arrivalProcess = new ArrivalProcess(arrivalRandomStream, arrivalRate);
+		locationStream = locationRandomStream;
+	}
     
     public void handleArrival() {
-        // process a new arrival
+        // create and process a new accident
     }
-    
+
+    // returns a random location inside the region
     public double[] drawLocation() {
         // determine the location of the accident
         double[] location = new double[2];
-        
-        // SB: locations of arriving emergency calls are chosen uniform in their region
-        // SB: diameter of the hexagon (region) is 10km
-        // SB: coordinates can be chosen uniform over the surface of the hexagon. 
-        
-        /*
-         * even in NL:
-         * de diameter is 10, dan lijkt mij dat de straal 5 is.
-         * oppervlakte is dan: (3/2) * wortel(3) * 5^2
-         */
-        
-        double surfaceRegion = (3./2) * Math.sqrt(3) * Math.pow(5, 2);
-        
-        // SB: how can we choose uniformly between zero and surface?
-        
         location[0] = 0.0; // X-Coordinate of accident location
         location[1] = 0.0; // Y-Coordinate of accident location
         return location;
     }
     
-    class ArrivalProcess extends Event {
-        ExponentialGen arrivalTimeGen;
-        double arrivalRate;
+	class ArrivalProcess extends Event {
 
-        public ArrivalProcess(RandomStream rng, double arrivalRate) {
-            this.arrivalRate = arrivalRate;
-            arrivalTimeGen = new ExponentialGen(rng, arrivalRate);
-        }
-        
-        @Override
-        public void actions() {
-            double nextArrival = arrivalTimeGen.nextDouble();
-            schedule(nextArrival);//Schedule this event after
-            //nextArrival time units
-            handleArrival();
-        }
-        
-        public void init() {
-            double nextArrival = arrivalTimeGen.nextDouble();
-            schedule(nextArrival);//Schedule this event after
-            //nextArrival time units
-        }
-    }
-    
+		ExponentialGen arrivalTimeGen;
+		double arrivalRate;
+
+		public ArrivalProcess(RandomStream rng, double arrivalRate) {
+			this.arrivalRate = arrivalRate;
+			arrivalTimeGen = new ExponentialGen(rng, arrivalRate);
+		}
+
+		// event: new customer arrival at the store
+		@Override
+		public void actions() {
+			handleArrival();
+			double nextArrival = arrivalTimeGen.nextDouble();
+			schedule(nextArrival); // schedule a new arrival event
+		}
+
+		public void init() {
+			double nextArrival = arrivalTimeGen.nextDouble();
+			schedule(nextArrival); // schedule a first new arrival
+		}
+	}
 }
